13785e3e7cdd7db856df6ec3bdab0826a3c3dea4
Matt Thornberry <mtberry16@hotmail.com>
2019-12-11 16:00:49 -0700

    Seed database

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,8 @@
+INSERT INTO notes (note_name, note_text, folderId)
+VALUES
+    ('Dogs', 'Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. Velit ex animi reiciendis quasi.', '1'),
+    ('Cats', 'Eos laudantium quia ab blanditiis temporibus necessitatibus. Culpa et voluptas ut sed commodi.', '1'),
+    ('Pigs', 'Occaecati dignissimos quam qui facere deserunt quia. Quaerat aut eos laudantium dolor odio officiis illum.', '2'),
+    ('Bears', 'Distinctio dolor nihil ad iure quo tempore id ipsum. Doloremque sunt dicta odit. Id veritatis aut et doloremque natus.', '2'),
+    ('Birds', 'Eum culpa odit. Veniam porro molestiae dolores sunt reiciendis culpa. Atque accusamus dolore eos odio facilis.', '3'),
+    ('Horses', 'Aliquid magnam ut quis quas impedit molestiae laudantium adipisci et. Officiis ut dolor rerum molestiae. Natus rerum libero aperiam. Rem aut consequatur.', '3');
