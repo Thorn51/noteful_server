242a1e43590d2718d9d4be34d5ddc23077eec657
Matt Thornberry <mtberry16@hotmail.com>
2019-12-12 10:14:27 -0700

    Build methods for notes service

---
+++ src/notes/notes-services.js
@@ -0,0 +1,35 @@
+const NotesService = {
+  getAllNotes(knex) {
+    return knex.select("*").from("notes");
+  },
+  insertNote(knex, newNote) {
+    return knex
+      .insert(newNote)
+      .into("notes")
+      .returning("*")
+      .then(rows => {
+        return rows[0];
+      });
+  },
+  getById(knex, id) {
+    return knex
+      .from("notes")
+      .select("*")
+      .where("id", id)
+      .first();
+  },
+  deleteNote(knex, id) {
+    return knex
+      .from("notes")
+      .where("id", id)
+      .delete();
+  },
+  updateNote(knex, id, updateNoteFields) {
+    return knex
+      .from("notes")
+      .where({ id })
+      .update(updateNoteFields);
+  }
+};
+
+module.exports = NotesService;
