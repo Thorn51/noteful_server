f640343f6335e0b69ca77e3fa6818f1b3b70fad7
Matt Thornberry <mtberry16@hotmail.com>
2019-12-12 10:15:08 -0700

    Create notes test data

---
+++ test/notes.fixtures.js
@@ -0,0 +1,54 @@
+function makeNotesArray() {
+  return [
+    {
+      id: 1,
+      note_name: "Test Note 1",
+      note_text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      modified: new Date(),
+      folderid: 1
+    },
+    {
+      id: 2,
+      note_name: "Test Note 2",
+      note_text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      modified: new Date(),
+      folderid: 2
+    },
+    {
+      id: 3,
+      note_name: "Test Note 3",
+      note_text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      modified: new Date(),
+      folderid: 3
+    },
+    {
+      id: 4,
+      note_name: "Test Note 4",
+      note_text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      modified: new Date(),
+      folderid: 1
+    },
+    {
+      id: 5,
+      note_name: "Test Note 5",
+      note_text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      modified: new Date(),
+      folderid: 2
+    },
+    {
+      id: 6,
+      note_name: "Test Note 6",
+      note_text:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      modified: new Date(),
+      folderid: 3
+    }
+  ];
+}
+
+module.exports = { makeNotesArray };
