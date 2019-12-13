0ba87839d2c763b68a3ceebb4242b086a3b4fd5d
Matt Thornberry <mtberry16@hotmail.com>
2019-12-13 16:28:58 +0000

    Change date to string

--- test/folders.fixtures.js
+++ test/folders.fixtures.js
@@ -3,17 +3,17 @@ function makeFoldersArray() {
     {
       id: 1,
       folder_name: "Test Folder 1",
-      date_created: new Date()
+      date_created: "2010-09-05T16:28:32.615Z"
     },
     {
       id: 2,
       folder_name: "Test Folder 2",
-      date_created: new Date()
+      date_created: "2018-03-05T16:28:32.615Z"
     },
     {
       id: 3,
       folder_name: "Test folder 3",
-      date_created: new Date()
+      date_created: "2019-12-05T16:28:32.615Z"
     }
   ];
 }
